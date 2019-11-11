26126d1fc99a659624aecfe9321707dc1ec86326
Kelly DeMent <kdement@ufl.edu>
2019-11-11 11:50:01 -0500

    Add code to volunteer page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//------------------MHAMBURGER MENU---------------------//
+
 var hamburgerButton = document.getElementById('hamburger');
 var navMenu = document.querySelector('.menu');
 
@@ -11,6 +13,9 @@ hamburgerButton.addEventListener('click', toggleMenu);
 var slideOutAd = document.getElementById('slideout-ad');
 
 
+
+//-----------------SLIDE OUT AD--------------------//
+
 // slide out after 3 seconds
 function sideAd () {
   slideOutAd.classList.remove('hide');
@@ -18,3 +23,43 @@ function sideAd () {
 };
 
 h3.addEventListener('click', sideAd);
+
+
+
+
+//------------------ACCORDION---------------------//
+
+//Open and close sign
+var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
+var accordionDescriptions = document.querySelectorAll('#accordion .accordion-description');
+
+//Handles the accordion dropdown action
+function expandAccordionDescription () {
+
+  var box = this.nextElementSibling;
+  var toggleIndicator = this.childNodes[3];
+
+  //If box is open, close box on click and change indicator
+  if (box.classList.contains('active')) {
+    box.classList.remove('active');
+    toggleIndicator.innerHTML = '+';
+  }
+
+  //If box is closed, close all boxes on click and change indicator
+  else {
+    for (var i = 0; i < accordionHeaders.length; i++) {
+      accordionDescriptions[i].classList.remove('active');
+      accordionHeaders[i].childNodes[3].innerHTML = '+';
+    }
+
+  //Then open clicked box and change indicator
+    box.classList.add('active');
+    toggleIndicator.innerHTML = '-';
+  }
+
+}
+
+//Add cick event listener for accordion headers
+for (i=0; i<accordionHeaders.length; i++) {
+  accordionHeaders[i].addEventListener('click', expandAccordionDescription);
+}
